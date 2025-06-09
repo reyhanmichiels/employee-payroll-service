46ccdd557c515a82206466052f31fd717940aac4
reyhanmichiels <michiels.reyhan@gmail.com>
2025-06-09 15:33:51 +0700

    domain for attendance overtime

---
+++ src/business/domain/overtime/overtime_query.go
@@ -0,0 +1,56 @@
+package overtime
+
+const (
+	insertOvertime = `
+		INSERT INTO overtimes (
+			fk_user_id,
+			overtime_date,
+			overtime_hour,
+			approved_date,
+			approved_by,
+			created_at,
+			created_by
+		) VALUES (
+			:fk_user_id,
+			:overtime_date,
+			:overtime_hour,
+			:approved_date,
+			:approved_by,
+			:created_at,
+			:created_by
+		)
+	`
+
+	readOvertime = `
+		SELECT
+			id,
+			fk_user_id,
+			overtime_date,
+			overtime_hour,
+			approved_date,
+			approved_by,
+			status,
+			flag,
+			meta,
+			created_at,
+			created_by,
+			updated_at,
+			updated_by,
+			deleted_at,
+			deleted_by
+		FROM
+			overtimes
+	`
+
+	countOvertime = `
+		SELECT
+			COUNT(*)
+		FROM
+			overtimes
+	`
+
+	updateOvertime = `
+		UPDATE
+			overtimes
+	`
+)
