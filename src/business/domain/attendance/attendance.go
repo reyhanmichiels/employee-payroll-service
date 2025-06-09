1ad2e95a19b72ba913d476e6362327acf8442a29
reyhanmichiels <michiels.reyhan@gmail.com>
2025-06-09 15:33:24 +0700

    domain for attendance

---
+++ src/business/domain/attendance/attendance.go
@@ -0,0 +1,130 @@
+package attendance
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/reyhanmichiels/go-pkg/v2/errors"
+	"github.com/reyhanmichiels/go-pkg/v2/log"
+	"github.com/reyhanmichiels/go-pkg/v2/parser"
+	"github.com/reyhanmichiels/go-pkg/v2/redis"
+	"github.com/reyhanmichiels/go-pkg/v2/sql"
+	"github.com/reyhanmichies/go-rest-api-boiler-plate/src/business/entity"
+)
+
+type Interface interface {
+	Get(ctx context.Context, param entity.AttendanceParam) (entity.Attendance, error)
+	GetList(ctx context.Context, param entity.AttendanceParam) ([]entity.Attendance, *entity.Pagination, error)
+	Create(ctx context.Context, param entity.AttendanceInputParam) (entity.Attendance, error)
+	Update(ctx context.Context, updateParam entity.AttendanceUpdateParam, selectParam entity.AttendanceParam) error
+}
+
+type attendance struct {
+	db    sql.Interface
+	log   log.Interface
+	redis redis.Interface
+	json  parser.JSONInterface
+}
+
+type InitParam struct {
+	Db    sql.Interface
+	Log   log.Interface
+	Redis redis.Interface
+	Json  parser.JSONInterface
+}
+
+func Init(param InitParam) Interface {
+	return &attendance{
+		db:    param.Db,
+		log:   param.Log,
+		redis: param.Redis,
+		json:  param.Json,
+	}
+}
+
+func (a *attendance) Get(ctx context.Context, param entity.AttendanceParam) (entity.Attendance, error) {
+	attendance := entity.Attendance{}
+
+	marshalledParam, err := a.json.Marshal(param)
+	if err != nil {
+		return attendance, err
+	}
+
+	if !param.BypassCache {
+		attendance, err = a.getCache(ctx, fmt.Sprintf(getAttendanceByKey, string(marshalledParam)))
+		switch {
+		case errors.Is(err, redis.Nil):
+			a.log.Warn(ctx, fmt.Sprintf(entity.ErrorRedisNil, err.Error()))
+		case err != nil:
+			a.log.Warn(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+		default:
+			return attendance, nil
+		}
+	}
+
+	attendance, err = a.getSQL(ctx, param)
+	if err != nil {
+		return attendance, err
+	}
+
+	err = a.upsertCache(ctx, fmt.Sprintf(getAttendanceByKey, string(marshalledParam)), attendance, a.redis.GetDefaultTTL(ctx))
+	if err != nil {
+		a.log.Error(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+	}
+
+	return attendance, nil
+}
+
+func (a *attendance) GetList(ctx context.Context, param entity.AttendanceParam) ([]entity.Attendance, *entity.Pagination, error) {
+	if !param.BypassCache {
+		attendanceList, pg, err := a.getCacheList(ctx, param)
+		switch {
+		case errors.Is(err, redis.Nil):
+			a.log.Warn(ctx, fmt.Sprintf(entity.ErrorRedisNil, err.Error()))
+		case err != nil:
+			a.log.Warn(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+		default:
+			return attendanceList, &pg, nil
+		}
+	}
+
+	attendanceList, pg, err := a.getListSQL(ctx, param)
+	if err != nil {
+		return attendanceList, pg, err
+	}
+
+	err = a.upsertCacheList(ctx, param, attendanceList, *pg, a.redis.GetDefaultTTL(ctx))
+	if err != nil {
+		a.log.Error(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+	}
+
+	return attendanceList, pg, nil
+}
+
+func (a *attendance) Create(ctx context.Context, param entity.AttendanceInputParam) (entity.Attendance, error) {
+	attendance, err := a.createSQL(ctx, param)
+	if err != nil {
+		return attendance, err
+	}
+
+	err = a.deleteCache(ctx, deleteAttendanceKeysPattern)
+	if err != nil {
+		a.log.Error(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+	}
+
+	return attendance, nil
+}
+
+func (a *attendance) Update(ctx context.Context, updateParam entity.AttendanceUpdateParam, selectParam entity.AttendanceParam) error {
+	err := a.updateSQL(ctx, updateParam, selectParam)
+	if err != nil {
+		return err
+	}
+
+	err = a.deleteCache(ctx, deleteAttendanceKeysPattern)
+	if err != nil {
+		a.log.Error(ctx, fmt.Sprintf(entity.ErrorRedis, err.Error()))
+	}
+
+	return nil
+}
---
+++ src/business/entity/attendance.go
@@ -0,0 +1,47 @@
+package entity
+
+import (
+	"github.com/reyhanmichiels/go-pkg/v2/null"
+	"github.com/reyhanmichiels/go-pkg/v2/query"
+)
+
+type Attendance struct {
+	ID                 int64     `db:"id" json:"id"`
+	AttendancePeriodID int64     `db:"fk_attendance_period_id" json:"attendancePeriodID"`
+	UserID             int64     `db:"fk_user_id" json:"userID"`
+	AttendanceDate     null.Date `db:"attendance_date" json:"attendanceDate"`
+
+	// Utility Column
+	Status    int64       `db:"status" json:"status"`
+	Flag      int64       `db:"flag" json:"flag,omitempty"`
+	Meta      null.String `db:"meta" json:"meta,omitempty" swaggertype:"string"`
+	CreatedAt null.Time   `db:"created_at" json:"createdAt" swaggertype:"string" example:"2022-06-21T10:32:29Z"`
+	CreatedBy null.Int64  `db:"created_by" json:"createdBy" swaggertype:"string"`
+	UpdatedAt null.Time   `db:"updated_at" json:"updatedAt" swaggertype:"string" example:"2022-06-21T10:32:29Z"`
+	UpdatedBy null.String `db:"updated_by" json:"updatedBy" swaggertype:"string"`
+	DeletedAt null.Time   `db:"deleted_at" json:"deletedAt,omitempty" swaggertype:"string" example:"2022-06-21T10:32:29Z"`
+	DeletedBy null.String `db:"deleted_by" json:"deletedBy,omitempty" swaggertype:"string"`
+}
+
+type AttendanceInputParam struct {
+	AttendancePeriodID int64      `db:"fk_attendance_period_id" json:"attendancePeriodID"`
+	UserID             int64      `db:"fk_user_id" json:"userID"`
+	AttendanceDate     null.Date  `db:"attendance_date" json:"attendanceDate"`
+	CreatedAt          null.Time  `db:"created_at" json:"-"`
+	CreatedBy          null.Int64 `db:"created_by" json:"-"`
+}
+
+type AttendanceUpdateParam struct {
+	AttendancePeriodID int64      `db:"fk_attendance_period_id" json:"attendancePeriodID"`
+	AttendanceDate     null.Date  `db:"attendance_date" json:"attendanceDate"`
+	Status             null.Int64 `db:"status" json:"status"`
+	UpdatedAt          null.Time  `db:"updated_at" json:"-"`
+	UpdatedBy          null.Int64 `db:"updated_by" json:"-"`
+}
+
+type AttendanceParam struct {
+	ID          int64 `db:"id" param:"id" json:"id"`
+	QueryOption query.Option
+	BypassCache bool
+	PaginationParam
+}
